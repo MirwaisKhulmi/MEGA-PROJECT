ff02affcc5ae61f016ac3294b47a99b7da658a34
Mrx <eyblish@gmail.com>
2022-07-28 06:38:53 +0600

    Add files via upload

---
+++ gettoken.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import requests, re
+
+logo = """
+
+ [*] ----------------------------------------------
+ [*] Gitbub      : https://github.com/kapokcode
+ [*] Facebook    : https://www.facebook.com/surachai.4452
+ [*] ----------------------------------------------\n"""
+
+print(logo)
+print('\n** < GET FB ACCESS TOKEN FROM COOKIE > **\n')
+cookie = input('* Cookie? : ')
+try:
+    data = requests.get('https://business.facebook.com/business_locations', headers = {
+        'user-agent'                : 'Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36', # don't change this user agent.
+        'referer'                   : 'https://www.facebook.com/',
+        'host'                      : 'business.facebook.com',
+        'origin'                    : 'https://business.facebook.com',
+        'upgrade-insecure-requests' : '1',
+        'accept-language'           : 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control'             : 'max-age=0',
+        'accept'                    : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'content-type'              : 'text/html; charset=utf-8'
+    }, cookies = {
+        'cookie'                    : cookie
+    })
+    find_token = re.search('(EAAG\w+)', data.text)
+    results    = '\n* Fail : maybe your cookie invalid !!' if (find_token is None) else '\n* Your fb access token : ' + find_token.group(1)
+except requests.exceptions.ConnectionError:
+    results    = '\n* Fail : no connection here !!'
+except:
+    results    = '\n* Fail : unknown errors, please try again !!'
+
+print(results)
